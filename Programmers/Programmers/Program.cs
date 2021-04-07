be3907f3ae2357cee42e4084a3f4d1071702fa99
ParkHD <76206050+ParkHD@users.noreply.github.com>
2021-04-07 23:47:17 +0900

    0407

---
+++ Programmers/Programmers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+namespace Programmers
+{
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] digits = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" }; 
+            digits = digits.Where((x, index) => x.Length < index).ToArray();
+
+            Console.WriteLine(digits[0]);
+        }
+    }
+}
